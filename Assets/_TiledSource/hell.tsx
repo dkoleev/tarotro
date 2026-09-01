f9db512acbd7c8d6c7cbb1a733c0ee5a4be1345f
dmitry.koleev <d.koleev@crazypandagames.com>
2026-09-01 12:06:16 +0300

    tiled wip

---
+++ Assets/_TiledSource/hell.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="hell" tilewidth="32" tileheight="32" tilecount="153" columns="17">
+ <image source="hell_tiles.png" width="544" height="288"/>
+</tileset>
