01e25d4249a6680024512512c67c67b361d676e3
dmitry.koleev <d.koleev@crazypandagames.com>
2026-09-01 11:55:26 +0300

    tiled wip

---
+++ Assets/_TiledSource/hell_back_light.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="hell_back_light" tilewidth="1024" tileheight="346" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="hell_back_light/back layer.png" width="1024" height="346"/>
+ </tile>
+ <tile id="1">
+  <image source="hell_back_light/Background layer.png" width="1024" height="346"/>
+ </tile>
+ <tile id="3">
+  <image source="hell_back_light/front layer.png" width="1024" height="346"/>
+ </tile>
+ <tile id="4">
+  <image source="hell_back_light/middle layer.png" width="1024" height="346"/>
+ </tile>
+</tileset>
