95413bc8621f00e2674cd6c83183796c5b62e188
dmitry.koleev <d.koleev@crazypandagames.com>
2026-09-02 14:25:47 +0300

    dark level tiled

---
+++ Assets/_TiledSource/hell_back_dark.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="hell_back_dark" tilewidth="1024" tileheight="346" tilecount="4" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="hell_back_dark/back layer.png" width="1024" height="346"/>
+ </tile>
+ <tile id="1">
+  <image source="hell_back_dark/Background layer.png" width="1024" height="346"/>
+ </tile>
+ <tile id="2">
+  <image source="hell_back_dark/front layer.png" width="1024" height="346"/>
+ </tile>
+ <tile id="3">
+  <image source="hell_back_dark/middle layer.png" width="1024" height="346"/>
+ </tile>
+</tileset>
