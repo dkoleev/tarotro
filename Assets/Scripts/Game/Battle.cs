5381eccdc3fcfcbf2215a37d81662c21cddfd2a7
dmitry.koleev <d.koleev@crazypandagames.com>
2026-09-08 20:19:43 +0300

    steam sdk facepunch

--- Assets/Scripts/Game/Battle.cs
+++ Assets/Scripts/Game/Battle.cs
@@ -1,7 +1,21 @@
-﻿namespace Game
+﻿using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+namespace Game
 {
-    public class Battle
-    {
+    public class Battle : MonoBehaviour {
+        [SerializeField] private string enemyPrefabPath;
         
+        private void Start() {
+            SpawnEnemy().Forget();
+        }
+
+        private async UniTask SpawnEnemy() {
+            var handle = Addressables.InstantiateAsync(enemyPrefabPath, null, true, true);
+            var enemyGo = await handle.Task;
+            var enemy = enemyGo.GetComponent<Enemy>();
+            Debug.Log(enemy.Health);
+        }
     }
 }
--- Assets/Scripts/Game/Boot.cs
+++ Assets/Scripts/Game/Boot.cs
@@ -1,7 +1,15 @@
-namespace Game
-{
-    public class Boot
-    {
-        
+using System;
+using UnityEngine;
+
+namespace Game {
+    public class Boot : MonoBehaviour {
+        private void Start() {
+            try {
+                Steamworks.SteamClient.Init(252490);
+            }
+            catch (Exception e) {
+                Debug.LogException(e);
+            }
+        }
     }
 }
