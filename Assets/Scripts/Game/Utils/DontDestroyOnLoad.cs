72dd316fe5d417cca8855ebea2fdb4fa57215869
dmitry.koleev <dmitry.koleev@gmail.com>
2026-09-10 10:20:23 +0300

    fix

---
+++ Assets/Scripts/Game/Utils/DontDestroyOnLoad.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Game.Utils {
+    public class DontDestroyOnLoad : MonoBehaviour {
+        private void Awake() {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
